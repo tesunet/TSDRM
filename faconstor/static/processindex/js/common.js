d7a9efba769df78cece5dc5078488794a69ae262
eat1124 <68455969@qq.com>
2018-11-29 11:06:58 +0800

    界面调整

--- faconstor/static/processindex/js/common.js
+++ faconstor/static/processindex/js/common.js
@@ -28,8 +28,8 @@ var util = {
     },
     request: function () {
         $.ajax({
-            url: '/get_process_index_data/', //这里面是请求的接口地址
-            //url: '/static/processindex/data.json', //这里面是请求的接口地址
+            //url: '/get_process_index_data/', //这里面是请求的接口地址
+            url: '/static/processindex/data.json', //这里面是请求的接口地址
             type: 'POST',
             data: {
                 p_run_id: $("#process_run_id").val(),
