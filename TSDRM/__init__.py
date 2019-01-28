53a2164e4ffaa11237dc9f82b651c08bf89eeee6
miaokela <2972799448@qq.com>
2019-01-27 23:51:02 -0800

    linux

--- TSDRM/__init__.py
+++ TSDRM/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
 from .celery import app as celery_app
 import pymysql
-import win_unicode_console
+import sys
 
-win_unicode_console.enable()
+if sys.platform.startswith("win"):
+    import win_unicode_console
+
+    win_unicode_console.enable()
 
 pymysql.install_as_MySQLdb()
