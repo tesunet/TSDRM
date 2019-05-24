b7733961448f2abfa397038159408a11a9f363aa
miaokela <2972799448@qq.com>
2019-05-24 17:37:23 +0800

    docker相关操作

--- TSDRM/wsgi.py
+++ TSDRM/wsgi.py
@@ -6,15 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-import sys
-
-sys.path.append('/var/www/html/cloudDR')
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudDR.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TSDRM.settings")
 
 
 application = get_wsgi_application()
