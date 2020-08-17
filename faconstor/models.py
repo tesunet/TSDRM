6bc6f1ee09c1bcc59792a7018727a9acd996a044
miaokela <2972799448@qq.com>
2020-08-17 13:23:53 +0800

    Process模型修改

--- faconstor/models.py
+++ faconstor/models.py
@@ -40,6 +40,7 @@ class UserInfo(models.Model):
 
 
 class Process(models.Model):
+    pnode = models.ForeignKey('self', blank=True, null=True, related_name='children', verbose_name='父节点')
     code = models.CharField("预案编号", blank=True, max_length=50)
     name = models.CharField("预案名称", blank=True, max_length=50)
     remark = models.TextField("预案描述", blank=True, null=True)
